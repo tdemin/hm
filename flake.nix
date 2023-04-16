e3e2792bb5b8b2604cc11b0e070615db759cb429
Timur Demin <me@tdem.in>
2023-04-17 00:47:45 +0500

    Remove bloat and work-vendored programs

--- flake.nix
+++ flake.nix
@@ -4,26 +4,21 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         home-manager.url = "github:nix-community/home-manager/master";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
-        nixGL.url = "github:guibou/nixGL/main";
-        nixGL.inputs.nixpkgs.follows = "nixpkgs";
 
         vim-plug.url = "github:junegunn/vim-plug/master";
         vim-plug.flake = false;
         zsh-syntax-highlighting.url = "github:zsh-users/zsh-syntax-highlighting/master";
         zsh-syntax-highlighting.flake = false;
-
-        nixpkgsK8s.url = "github:NixOS/nixpkgs/bf972dc380f36a3bf83db052380e55f0eaa7dcb6";
     };
-    outputs = inputs@{ self, nixpkgs, home-manager, nixGL,
-        vim-plug, zsh-syntax-highlighting, nixpkgsK8s, ... }: {
+    outputs = inputs@{ self, nixpkgs, home-manager,
+        vim-plug, zsh-syntax-highlighting, ... }: {
         homeConfigurations = let
             stateVersion = "22.11";
             genericConfiguration = { username, homeDirectory, system }:
-                let k8s = import nixpkgsK8s { inherit system; }; in
                 home-manager.lib.homeManagerConfiguration {
                     pkgs = nixpkgs.legacyPackages.${system};
                     extraSpecialArgs = {
-                        inherit nixGL vim-plug zsh-syntax-highlighting k8s;
+                        inherit vim-plug zsh-syntax-highlighting;
                     };
                     modules = [
                         ./home.nix
--- home.nix
+++ home.nix
@@ -1,18 +1,12 @@
 {
     pkgs,
-    nixGL,
 
     vim-plug,
     zsh-syntax-highlighting,
-    k8s,
     ...
 }:
 
 let
-    wrapGL = binaryName: path:
-        pkgs.writeShellScriptBin "${binaryName}" ''
-            ${nixGL.nixGLIntel}/bin/nixGLIntel ${path} "$@"
-        '';
     overrides = {
         akm = pkgs.callPackage ./packages/akm {};
         rescrobbled = pkgs.callPackage ./packages/rescrobbled {};
@@ -27,24 +21,16 @@ in rec {
         ];
     home.packages = with pkgs; [
         # tools
-        act
         age
-        overrides.akm
         amfora
-        android-tools
         ansible
         ansible-lint
-        argo
-        argocd
         aria2
         asdf-vm
         bat
-        buildah
         chezmoi
         direnv
-        docker-compose
         fd
-        ffmpeg
         fzf
         hugo
         gh
@@ -53,15 +39,11 @@ in rec {
         goreleaser
         go-task
         graphviz
-        istioctl
         jq
-        jupyter
         gomplate
         maim
         minikube
         minisign
-        mksh
-        mpris-scrobbler
         neovim
         patchelf
         restic
@@ -71,18 +53,12 @@ in rec {
         scc
         shellcheck
         shfmt
-        skopeo
         socat
         starship
         texlive.combined.scheme-full
         tmux
-        # FIXME: borked on nixpkgs from cfb43ad7b941d9c3606fb35d91228da7ebddbfc5 
-        # vagrant
         vim
-        xclip
-        xdotool
         yarn
-        youtube-dl
         yt-dlp
         zathura
         zsh
@@ -92,7 +68,13 @@ in rec {
         dejavu_fonts
         liberation_ttf
         mplus-outline-fonts.osdnRelease
-        nerdfonts
+        (nerdfonts.override { fonts = [
+            "DejaVuSansMono"
+            "DroidSansMono"
+            "FiraCode"
+            "RobotoMono"
+            "UbuntuMono"
+        ]; })
         noto-fonts
         noto-fonts-cjk
         noto-fonts-extra
@@ -103,9 +85,7 @@ in rec {
         source-sans-pro
         ubuntu_font_family
     ] ++ [
-        k8s.kubernetes-helm
-        k8s.kubectl
-        k8s.helmfile
+        overrides.akm
     ];
     home.extraOutputsToInstall = [ "doc" ];
 
